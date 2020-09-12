8f11a82d862c0e7b1146bb8ccb1788a6c6ccca77
Swartz <Phillip.Swartz@lfg.com>
2020-09-12 17:11:45 -0400

    initial commit for web weather app

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <input className="searchbar" type="text" placeholder="Search...">
+
+
+        </input>
+
+      </main>
+      
+    </div>
+  );
+}
+
+export default App;
