cd86fdb26516f65c1658bd0ac1b3b1cac179792d
Swartz <Phillip.Swartz@lfg.com>
2020-09-15 18:45:40 -0400

    formmating updates

--- src/App.js
+++ src/App.js
@@ -4,16 +4,30 @@ import './App.css';
 
 
 function App() {
+
+  const todayDate = new Date();
+  const todayDateFormat = todayDate.toDateString();
+
   return (
     <div className="App">
       <main>
+
+      <div className="searchbox">
         <input className="searchbar" type="text" placeholder="Search...">
 
 
         </input>
+      </div>
+
+        <div className="weather-location">
+          <div className="location">Philadelphia, PA</div>
+          <div className="date">{todayDateFormat}</div>
+
+        </div>
 
       </main>
-      
+
+
     </div>
   );
 }
