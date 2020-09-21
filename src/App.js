4c471963f4a7324468ade081d94bb2ba7185690b
phil <pswartz.sju@gmail.com>
2020-09-21 17:50:01 -0400

    added base api call for current weather, to update a forecast

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import keys from './key';
 
+const API = {
+  key: keys.APIKEY,
+  base_url: "http://api.openweathermap.org/data/2.5/"
+}
 
+// const url = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={YOUR API KEY}"
 
 function App() {
+  const [query, setQuery] = useState('')
+  // const [weather, setWeather] = useState({});
+
+  const search = (evnt) => {
+    if (evnt.key === "Enter") {
+      fetch(`${API.base_url}weather?q=${query}&units=metric&APPID=${API.key}`)
+        .then(res => res.json())
+        .then(result => {
+          // setWeather(result);
+          setQuery('');
+          console.log(result);
+      });
+    }
+  }
+
+  // fetch the forecast using a diff url
+  // const search = (evnt) => {
+  //   if (evnt.key === "Enter") {
+  //     fetch(`${API.base_url}weather?q=${query}&units=metric&APPID=${API.key}`)
+  //       .then(res => res.json())
+  //       .then(result => {
+  //         // setWeather(result);
+  //         setQuery('');
+  //         console.log(result);
+  //     });
+  //   }
+  // }
 
   const todayDate = new Date();
   const todayDateFormat = todayDate.toDateString();
@@ -13,8 +46,14 @@ function App() {
       <main>
 
       <div className="searchbox">
-        <input className="searchbar" type="text" placeholder="Search...">
-
+        <input 
+          className="searchbar" 
+          type="text" 
+          placeholder="Search..."
+          onChange={ e => setQuery(e.target.value)}
+          value={query}
+          onKeyPress={search}
+        >
 
         </input>
       </div>
