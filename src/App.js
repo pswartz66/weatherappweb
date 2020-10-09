a79a295bd9e331926048213bdc29d91fd4c10330
Swartz <Phillip.Swartz@lfg.com>
2020-10-08 23:53:20 -0400

    fetched api to display weather data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import keys from './key';
 
@@ -11,19 +11,20 @@ const API = {
 
 function App() {
   const [query, setQuery] = useState('')
-  // const [weather, setWeather] = useState({});
+  const [weather, setWeather] = useState({});
 
   const search = (evnt) => {
     if (evnt.key === "Enter") {
       fetch(`${API.base_url}weather?q=${query}&units=metric&APPID=${API.key}`)
         .then(res => res.json())
         .then(result => {
-          // setWeather(result);
+          setWeather(result);
           setQuery('');
           console.log(result);
-      });
+        });
     }
   }
+
 
   // fetch the forecast using a diff url
   // const search = (evnt) => {
@@ -45,34 +46,38 @@ function App() {
     <div className="App">
       <main>
 
-      <div className="searchbox">
-        <input 
-          className="searchbar" 
-          type="text" 
-          placeholder="Search..."
-          onChange={ e => setQuery(e.target.value)}
-          value={query}
-          onKeyPress={search}
-        >
+        <div className="searchbox">
+          <input
+            className="searchbar"
+            type="text"
+            placeholder="Search..."
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          >
 
-        </input>
-      </div>
-
-        <div className="weather-location">
-          <div className="location">Philadelphia, PA</div>
-          <div className="date">{todayDateFormat}</div>
-
+          </input>
         </div>
 
-        <div className="weather-data">
-          <div className="temperature">
-            15°c
+        {(typeof weather.main != "undefined") ? (
+          <div>
+            <div className="weather-location">
+              <div className="location">{`${weather.name}, ${weather.sys.country}`}</div>
+              <div className="date">{todayDateFormat}</div>
 
+            </div>
+
+            <div className="weather-data">
+              <div className="temperature">
+                {`${Math.round(weather.main.temp * (9/5) + 32)}°f`}
           </div>
-          <div className="description">
-            Sunny
+              <div className="description">
+                {`${weather.weather[0].main}`}
           </div>
-        </div>
+            </div>
+          </div>
+        ) : (null)}
+
 
       </main>
 
